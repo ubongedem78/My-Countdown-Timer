4a9760a782f1616b2948ca0fdc337f99ffa23b9a
Ubong's <Ubongedem78@gmail.com>
2022-09-11 21:27:23 +0100

    my first commit

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
 const anniversaryDate = new Date('20 June, 2023');
-const dayEl = document.getElementById("days");
-const hoursEl = document.getElementById('hours');
-const minutesEl = document.getElementById('minutes');
-const secondsEl = document.getElementById('seconds');
-
+const dayElement = document.getElementById('days');
+const hoursElement = document.getElementById('hours');
+const minutesElement = document.getElementById('minutes');
+const secondsElement = document.getElementById('seconds');
 function countdown(){
     const currentDate = new Date();
-    const totalSeconds = Math.abs(currentDate - anniversaryDate)/ 1000;
 
-    const noOfDays = Math.floor(totalSeconds/3600/24);
-    const noOfHours = Math.floor(totalSeconds/3600) % 24;
-    const noOfMinutes = Math.floor(totalSeconds/60) % 60;
-    const noOfSeconds = Math.floor(totalSeconds) % 60;
-    
-    dayEl.innerHTML = noOfDays;
-    hoursEl.innerHTML = formatTime(noOfHours);
-    minutesEl.innerHTML = formatTime(noOfMinutes);
-    secondsEl.innerHTML = formatTime(noOfSeconds);
+    const totalSeconds = Math.abs(anniversaryDate-currentDate)/1000;
+    const noOfDays = Math.floor(Math.abs(totalSeconds/3600/24));
+    const noOfHours = Math.floor(Math.abs(totalSeconds/3600) % 24);
+    const noOfMinutes = Math.floor(Math.abs(totalSeconds/60) % 60);
+    const noOfSeconds = Math.floor(Math.abs(totalSeconds)% 60);
 
+    dayElement.innerHTML = formatTime(noOfDays);
+    hoursElement.innerHTML = formatTime(noOfHours);
+    minutesElement.innerHTML = formatTime(noOfMinutes);
+    secondsElement.innerHTML = formatTime(noOfSeconds);
+   
 }
 
 function formatTime(time){
     if(time<10){
         return `0${time}`;
-    } 
-    return time;
+    } return time;
 }
 
-countdown();
 
+countdown();
 setInterval(countdown, 1000)
